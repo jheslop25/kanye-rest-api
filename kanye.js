e5a790129bf43c89cb2b23514474cbb73eded178
jheslop25 <john@earthmountain.ca>
2020-03-09 11:33:41 -0600

    added ajax request to kanye api

--- kanye.js
+++ kanye.js
@@ -13,3 +13,31 @@ class Kanye {
     }
 }
 
+var button = $('#kanyeREST');
+
+button.on(
+    {
+        "click": function(){
+            $.ajax(
+                {
+                    "url": "https://api.kanye.rest",
+                    "type": "GET",
+                    "data": {},
+                    "dateType": "JSON"
+                }
+            ).done(
+                function(resultJSON){
+                    console.log(resultJSON);
+                }
+            ).fail(
+                function(xhr, status, error){
+                    console.log('there was a problem ' + error);
+                }
+            ).always(
+                function(xhr, status){
+                    console.log('request complete with status: '+ status);
+                }
+            )
+        }
+    }
+)
