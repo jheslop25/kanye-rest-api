9c26e4aa6d1cae2227839c5962500c1b2ff3bd7f
jheslop25 <john@earthmountain.ca>
2020-03-09 11:27:54 -0600

    defined class kanye. added button to index.thml added jquery to project via CDN

---
+++ kanye.js
@@ -0,0 +1,15 @@
+class Kanye {
+    constructor(quote){
+        this.KanyeWis = quote;
+    }
+    speakDestiny(){
+        return this.KanyeWis;
+    }
+    shoutDestiny(){
+        return this.KanyeWis.toUpperCase() + "!!!!!!";
+    }
+    refuseDestiny(){
+        return this.KanyeWis + "...I recant my foolishness! SAVE ME OH LAWD!";
+    }
+}
+
